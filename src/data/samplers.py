78cfadecff63f3c59831de284b0f373329fe2177
vyzyv <vyz@protonmail.com>
2019-10-17 18:05:34 +0200

    Add sampler documentation

--- src/data/samplers.py
+++ src/data/samplers.py
@@ -4,12 +4,43 @@ from torch.utils.data import RandomSampler, Sampler, SubsetRandomSampler
 
 # Iterate over every dataset randomly
 class DatasetRandomSampler(SubsetRandomSampler):
+    """Standard Random Sampler but return indices from all datasets.
+
+    Example:
+    --------
+
+    For datasets of length [10000, 10000, 20000] will return sampler
+    yielding indices [0, 40000] (e.g. summed).
+
+    Yields
+    ------
+    int
+            Index into one of datasets.
+
+    """
+
     def __init__(self, *datasets):
         super().__init__(torch.arange(sum(map(len, datasets))))
 
 
 # Iterate randomly over first dataset, after that randomly through the second and so on...
 class TaskSampler(Sampler):
+    """Iterate randomly but in order of tasks provided.
+
+    Example:
+    --------
+
+    For datasets of length [10000, 10000, 20000] will return sampler
+    yielding random indices from range [0, 10000], followed by [100000, 200000]
+    and lastly [20000, 40000].
+
+    Yields
+    ------
+    int
+            Index into one of datasets.
+
+    """
+
     def __init__(self, *datasets):
         self.samplers = [RandomSampler(dataset) for dataset in datasets]
         self._length = sum(len(sampler) for sampler in self.samplers)
@@ -28,6 +59,27 @@ class TaskSampler(Sampler):
 
 # Return multiple indices, each for one dataset
 class JoinedSampler(Sampler):
+    """Return multiple indices each for one dataset.
+
+    Used by concatenation and mixup inputs (or any other where elements)
+    from datasets are needed at the same time.
+
+    Datasets will be sampled with replacement if they don't have the same
+    number of elements within them.
+
+    As this sampler yields tuples, it cannot be used with Dataset's __getitem__,
+    hence it is connected with dataset (see datasets module and _JoinedDataset base class).
+
+    Example:
+    --------
+
+    Yields
+    ------
+    Tuple[int]
+            Tuple containing indices into each of the dataset.
+
+    """
+
     # Each dataset shorter than the longest will be upsampled so number of samples will be equal.
     # This may induce overfitting on some datasets and underfitting on others.
     @classmethod
