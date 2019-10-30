c3ceac494a5e7475ebac648616287aedbba01909
Maciej Wolczyk <mwolczyk@szarancza.capdnet>
2019-10-30 02:02:54 +0100

    Changed the method of applying the split

--- src/options/split/_dev_utils.py
+++ src/options/split/_dev_utils.py
@@ -44,6 +44,36 @@ def get_tasks(args, model) -> int:
     """
     return list(model.modules())[-1].weight.shape[0] // args.labels
 
+def get_masks(args, tasks, masker: typing.Callable):
+    """Generate masks of task assignment for each layer
+
+    Parameters
+    ----------
+    args: argparse.Namespace
+            argparse.ArgumentParser().parse() return value. User provided arguments.
+    tasks: int
+            How many tasks were done within original network.
+    masker:
+            Object creating and applying masks to neural network layers
+
+    Yields
+    -------
+    list
+            List of torch tensors representing the masks.
+
+    """
+    with torch.no_grad():
+        model = get_model(args)
+        reversed_modules = reversed(list(model.modules()))
+        output_mask = torch.tensor(list(range(tasks)) * args.labels)
+        reversed_masks = [
+            masker(module) for module in reversed_modules
+            if nn.layers.spatial(module)
+        ]
+        hidden_masks = list(reversed(reversed_masks[:-1]))  # drop the last mask
+        masks = hidden_masks + [output_mask]
+    return masks
+
 
 def generate_networks(args, tasks, masker: typing.Callable):
     """Generate splitted per-task neural networks.
@@ -65,12 +95,18 @@ def generate_networks(args, tasks, masker: typing.Callable):
     """
     path = pathlib.Path(args.save)
     path.mkdir(parents=True, exist_ok=True)
-    with torch.no_grad():
-        for task in range(tasks):
-            model = get_model(args)
-            masker.reset()
-            for module in reversed(list(model.modules())):
-                if hasattr(module, "weight") and nn.layers.spatial(module):
-                    mask = masker(module)
-                    masker.apply(module.weight.data, mask, task)
-            torch.save(model, path / f"{task}.pt")
+
+    masks = get_masks(args, tasks, masker)
+
+    for task in range(tasks):
+        mask_idx = 0
+        model = get_model(args)
+        for module in model.modules():
+            if nn.layers.spatial(module):
+                if mask_idx == 0:  # skip the first layer
+                    mask_idx += 1
+                    continue
+
+                masker.apply(module.weight.data, masks[mask_idx], task)
+                mask_idx += 1
+        torch.save(model, path / f"{task}.pt")
